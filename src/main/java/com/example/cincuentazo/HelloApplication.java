7cec7a9cc73a07033ac9613f2e4e5a066baeb72f
luis <luisfglondono@gmail.com>
2024-12-07 22:07:25 -0500

    first commit, add window initial and styles

--- src/main/java/com/example/cincuentazo/HelloApplication.java
+++ src/main/java/com/example/cincuentazo/HelloApplication.java
@@ -1,8 +1,7 @@
 package com.example.cincuentazo;
 
+import com.example.cincuentazo.views.HelloView;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +9,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        HelloView.getInstance();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/cincuentazo/views/HelloView.java
+++ src/main/java/com/example/cincuentazo/views/HelloView.java
@@ -1,4 +1,35 @@
 package com.example.cincuentazo.views;
 
-public class HelloView {
+import com.example.cincuentazo.HelloApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloView extends Stage {
+    public HelloView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        this.setTitle("Cincuentazo - Inicio");
+        this.getIcons().add(new Image(
+                getClass().getResourceAsStream("/com/example/cincuentazo/images/icon.png")
+        ));
+        this.setScene(scene);
+        this.show();
+    }
+
+    private static class HelloViewHolder {
+        private static HelloView INSTANCE;
+    }
+
+    public static HelloView getInstance() throws IOException {
+
+        if (HelloViewHolder.INSTANCE == null) {
+            return HelloViewHolder.INSTANCE = new HelloView();
+        } else {
+            return HelloViewHolder.INSTANCE;
+        }
+    }
 }
