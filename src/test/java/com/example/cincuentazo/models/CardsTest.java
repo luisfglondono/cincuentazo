2b767d61510d9dfe5ddba6b26f34932a3e0acd7c
luis <luisfglondono@gmail.com>
2024-12-14 08:21:16 -0500

    Test add

--- src/test/java/com/example/cincuentazo/models/CardsTest.java
+++ src/test/java/com/example/cincuentazo/models/CardsTest.java
@@ -1,6 +1,31 @@
 package com.example.cincuentazo.models;
 
+import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 class CardsTest {
-  
+    @Test
+    void testGetCardValueWithA() {
+        Cards card = new Cards("Diamonds", 1, "A", 10);
+
+        assertEquals(1, card.getFirstValue());
+        assertEquals(10, card.getSecondValue());
+    }
+
+    @Test
+    void testGetCardValueWith9() {
+        Cards card = new Cards("Hearts", 9, "9", 0);
+
+        assertEquals(9, card.getFirstValue());
+    }
+
+    @Test
+    void testGetCardValueWithQ() {
+        Cards card = new Cards("Spades", -10, "Q", 0);
+
+        assertEquals(-10, card.getFirstValue());
+    }
+
+
 }
